4ea0505770531a1f937aedacacf0b33fb1ca9a8f
Estrella Abueva <estrella.abueva@cit.edu>
2022-12-15 11:59:29 +0800

    Add changes

--- Abueva_FuzzyLogic/Abueva_FuzzyLogic/Form1.Designer.cs
+++ Abueva_FuzzyLogic/Abueva_FuzzyLogic/Form1.Designer.cs
@@ -28,13 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_dirt = new System.Windows.Forms.Label();
+            this.strength = new System.Windows.Forms.Label();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_dirt
+            // 
+            this.lbl_dirt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_dirt.Location = new System.Drawing.Point(432, 207);
+            this.lbl_dirt.Name = "lbl_dirt";
+            this.lbl_dirt.Size = new System.Drawing.Size(100, 23);
+            this.lbl_dirt.TabIndex = 26;
+            this.lbl_dirt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_dirt.Click += new System.EventHandler(this.lbl_distance_Click);
+            // 
+            // strength
+            // 
+            this.strength.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.strength.Location = new System.Drawing.Point(432, 142);
+            this.strength.Name = "strength";
+            this.strength.Size = new System.Drawing.Size(100, 23);
+            this.strength.TabIndex = 25;
+            this.strength.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lbl_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_result.Location = new System.Drawing.Point(178, 352);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(459, 57);
+            this.lbl_result.TabIndex = 24;
+            this.lbl_result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(174, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(340, 24);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "New Strength of Vacuuming should be:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(341, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 31);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Defuzzify";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(594, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Fuzzify";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(381, 168);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(195, 45);
+            this.trackBar2.TabIndex = 20;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(160, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 24);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Dirt";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(594, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Fuzzify";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(156, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 24);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Strength of Vacuuming";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(381, 101);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(195, 45);
+            this.trackBar1.TabIndex = 16;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(155, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(461, 25);
+            this.label1.TabIndex = 15;
+            this.label1.Text = " Fuzzy Logic to Control the Speed of a Car";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_dirt);
+            this.Controls.Add(this.strength);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_dirt;
+        private System.Windows.Forms.Label strength;
+        private System.Windows.Forms.Label lbl_result;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Abueva_FuzzyLogic/Abueva_FuzzyLogic/Form1.cs
+++ Abueva_FuzzyLogic/Abueva_FuzzyLogic/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotFuzzy;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,120 @@ namespace Abueva_FuzzyLogic
 {
     public partial class Form1 : Form
     {
+        FuzzyEngine fe;
+        MembershipFunctionCollection strengthVacuum, dirt, newStrength;
+        LinguisticVariable myStrengthVacuum, myDirt, myNewStrength;
+        FuzzyRuleCollection myRules;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void setMembers()
+        {
+            strengthVacuum.Add(new MembershipFunction("Slow", 0.0, 0.0, 10.0, 30.0));
+            strengthVacuum.Add(new MembershipFunction("Medium", 25.0, 45.0, 50.0, 65.0));
+            strengthVacuum.Add(new MembershipFunction("Fast", 60.0, 65.0, 100.0, 100.0));
+            myStrengthVacuum = new LinguisticVariable("STRENGTH OF VACUUMING", strengthVacuum);
+
+            dirt = new MembershipFunctionCollection();
+            dirt.Add(new MembershipFunction("Less", 0.0, 0.0, 10.0, 30.0));
+            dirt.Add(new MembershipFunction("Moderate", 25.0, 45.0, 50.0, 65.0));
+            dirt.Add(new MembershipFunction("High", 60.0, 65.0, 100.0, 100.0));
+            myDirt = new LinguisticVariable("DIRT IN THE AREA", dirt);
+
+            newStrength = new MembershipFunctionCollection();
+            newStrength.Add(new MembershipFunction("Slow", 0.0, 0.0, 10.0, 30.0));
+            newStrength.Add(new MembershipFunction("Medium", 25.0, 45.0, 50.0, 65.0));
+            newStrength.Add(new MembershipFunction("Fast", 60.0, 65.0, 100.0, 100.0));
+            myNewStrength = new LinguisticVariable("NEW STRENGTH OF VACUUMING", newStrength);
+        }
+
+        public void setRules()
+        {
+            myRules = new FuzzyRuleCollection();
+            myRules.Add(new FuzzyRule("IF (DIRT IN THE AREA IS Less) AND (STRENGTH OF VACUUMING IS Slow)  THEN NEW STRENGTH OF VACUUMING IS Slow"));
+            myRules.Add(new FuzzyRule("IF (DIRT IN THE AREA IS Less) AND (STRENGTH OF VACUUMING IS Medium)  THEN NEW STRENGTH OF VACUUMING IS Slow"));
+            myRules.Add(new FuzzyRule("IF (DIRT IN THE AREA IS Less) AND (STRENGTH OF VACUUMING IS Fast)  THEN NEW STRENGTH OF VACUUMING IS Medium"));
+            myRules.Add(new FuzzyRule("IF (DIRT IN THE AREA IS Moderate) AND (STRENGTH OF VACUUMING IS Slow)  THEN NEW STRENGTH OF VACUUMING IS Medium"));
+            myRules.Add(new FuzzyRule("IF (DIRT IN THE AREA IS Moderate) AND (STRENGTH OF VACUUMING IS Medium)  THEN NEW STRENGTH OF VACUUMING IS Medium"));
+            myRules.Add(new FuzzyRule("IF (DIRT IN THE AREA IS Moderate) AND (STRENGTH OF VACUUMING IS Fast)  THEN NEW STRENGTH OF VACUUMING IS Fast"));
+            myRules.Add(new FuzzyRule("IF (DIRT IN THE AREA IS High) AND (STRENGTH OF VACUUMING IS Slow)  THEN NEW STRENGTH OF VACUUMING IS Medium"));
+            myRules.Add(new FuzzyRule("IF (DIRT IN THE AREA IS High) AND (STRENGTH OF VACUUMING IS Medium)  THEN NEW STRENGTH OF VACUUMING IS Fast"));
+            myRules.Add(new FuzzyRule("IF (DIRT IN THE AREA IS High) AND (STRENGTH OF VACUUMING IS Fast)  THEN NEW STRENGTH OF VACUUMING IS Fast"));
+        }
+
+        public void setFuzzyEngine()
+        {
+            fe = new FuzzyEngine();
+            fe.LinguisticVariableCollection.Add(myStrengthVacuum);
+            fe.LinguisticVariableCollection.Add(myDirt);
+            fe.LinguisticVariableCollection.Add(myNewStrength);
+            fe.FuzzyRuleCollection = myRules;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setMembers();
+            setRules();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            strength.Text = trackBar1.Value.ToString();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            lbl_dirt.Text = trackBar2.Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myDirt.InputValue = double.Parse(lbl_dirt.Text);
+            myDirt.Fuzzify("High");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myStrengthVacuum.InputValue = double.Parse(strength.Text);
+            myStrengthVacuum.Fuzzify("Slow");
+        }
+        
+        private void lbl_distance_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            setFuzzyEngine();
+            fe.Consequent = "NEW STRENGTH OF VACUUMING";
+            lbl_result.Text = "" + fe.Defuzzify();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            myDirt.InputValue = trackBar2.Value;
+        }
+
+        public void FuzzifyValues()
+        {
+            myStrengthVacuum.InputValue = trackBar1.Value;
+            myStrengthVacuum.Fuzzify("Slow");
+            myDirt.InputValue = trackBar2.Value;
+            myDirt.Fuzzify("Less");
         }
     }
 }
